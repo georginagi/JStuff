94908ebf0ef6508cf255450da6fdd4dfa8e55d96
Georgina Giannoukou <g.giannoukou@gmail.com>
2017-10-26 16:12:07 +0200

    Update script.js

--- #2task/js/script.js
+++ #2task/js/script.js
@@ -6,7 +6,7 @@ function loadPage(pageID) {
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("main").innerHTML = xhttp.responseText
+            document.getElementById("mm").innerHTML = xhttp.responseText
         }
     }
     
